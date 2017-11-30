154b2744e2c63d38e002cd6d734c72774b447f85
Ashwin <itizashwin@gmail.com>
2017-11-30 21:51:05 +0530

    All operations done - working code

--- resouceAllocator.py
+++ resouceAllocator.py
@@ -1,3 +1,4 @@
+import sys
 # creating a dictionary with the processor count for each server type
 cpuCount={
 	'large'  :1,
@@ -7,6 +8,7 @@ cpuCount={
 	'8xlarge':16,
 	'10xlarge':32
 }
+maxDiff = sys.maxsize
 # Assuming the non-specified information in case of CPU count or 
 # price ( case 1 and 2 ) is represented as -1
 def get_costs(instances,hours,cpus,price):
@@ -22,11 +24,13 @@ def allocateCPU(instances,hours,cpus,price,state):
 	for instance in instances:
 		instanceSet = sorted(instances[instance],key=instances[instance].get,reverse=True);
 		finalList = []
+		global maxDiff
+		maxDiff = sys.maxsize
 		CostCalculator(instances[instance],instanceSet,hours,cpus,price,[],finalList,0,state)
 		server_list,total_cost = getListData(instances[instance],finalList,hours);
 		instanceDict = {
 			"region":instance,
-			"total_cost":str("$"+str(total_cost)),
+			"total_cost":str("$"+str(round(total_cost,2))),
 			"servers": server_list
 		}
 		result.append(instanceDict)
@@ -34,22 +38,35 @@ def allocateCPU(instances,hours,cpus,price,state):
 	return result
 
 def CostCalculator(instance,instanceSet,hours,cpus,price,li,finalList,start,state):
-	if state in [0,1]:
-		variable =  price if state == 0 else cpus
-		if variable == 0:
+	if state == 1:
+		if cpus == 0:
 			if len(finalList)==0:
 				finalList.append(li[:])
 			return
-		if variable < 0:
+		if cpus < 0:
+			return
+	elif state == 0:
+		if price >= 0:
+			global maxDiff
+			if maxDiff > price - 0:
+				if len(finalList)>0:
+					finalList.pop()
+				maxDiff = price - 0
+				finalList.append(li[:])
+		if price < 0:
 			return
 	else:
 		if cpus == 0 and price >= 0:
 			if len(finalList)==0:
 				finalList.append(li[:])
+				myNewList = []
+				CostCalculator(instance,instanceSet,hours,cpus,price,[],myNewList,0,0)
+				for i in myNewList:
+					finalList.append(i)
 		if cpus < 0 or price < 0:
 			return 
 	for i in range(start,len(instance)):
-		if len(finalList)>0:
+		if len(finalList)>0 and state != 0:
 			break;
 		li.append(instanceSet[i])
 		if state == 0:
@@ -74,7 +91,6 @@ def getListData(instance,finalList,hours):
 		for key,value in ServerDict.items():
 			serverList.append(tuple([key,value]))
 		return serverList,total_cost*hours
-
 
 
 
