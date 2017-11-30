6f758e3d23f23cd0c5b9a17f78fa4fbb02d0efcb
Ashwin <itizashwin@gmail.com>
2017-11-30 12:30:14 +0530

    new one

--- resouceAllocator.py
+++ resouceAllocator.py
@@ -75,24 +75,6 @@ def getListData(instance,finalList,hours):
 			serverList.append(tuple([key,value]))
 		return serverList,total_cost*hours
 
-myDict = {
-	"us-east":   {
-                   "large":   0.12,
-                   "xlarge":   0.23,
-                   "2xlarge":   0.45,                    
-                   "4xlarge":   0.774,                    
-                   "8xlarge":   1.4,
-                "10xlarge":   2.82
-                },
-    
-    "us-west":   {
-                   "large":   0.14,
-                   "2xlarge":   0.413,                    
-                   "4xlarge":   0.89,                    
-                   "8xlarge":   1.3,
-             }  
-}
-print(get_costs(myDict,24,-1,169.272))
 
 
 
